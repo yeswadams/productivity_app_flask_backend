4a6f18aab01eccf7dc6a5526e0fd951ae1482d15
yeswadams <yeswaadams73@gmail.com>
2026-08-01 17:31:57 +0300

    implemented the application factory file and initiated all the extensions

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,4 +3,22 @@ from flask import Flask
 
 # config
 from app.core.config import DevelopmentConfig
-from app.extensions import d
+from app.extensions import bcrypt
+from app.extensions import db
+from app.extensions import jwt
+from app.extensions import migrate
+
+def create_app(config_class=DevelopmentConfig):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    # initializes the extensions
+    db.init_app(app, db)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+    bcrypt.init_app(app)
+
+    return app
+
+    
+    
