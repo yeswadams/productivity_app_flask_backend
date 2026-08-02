d2b9c5cac98d261e899066bb37747802e4b8cca0
yeswadams <yeswaadams73@gmail.com>
2026-08-02 21:16:48 +0300

    Implemented the PasswordResetToken Model for the app

--- backend/app/features/auth/models/reset_token.py
+++ backend/app/features/auth/models/reset_token.py
@@ -1,12 +1,21 @@
 from app.extensions.database import db
-from datetime import datetime
+from datetime import datetime, timezone
 
 class PasswordResetToken(db.model):
     __tablename__ = 'password_reset_tokens'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     token = db.Columnn(db.String(100), nullbale=False)
-    expiration = db.Column(db.Datetime, nullable=False)
+    is_used = db.Column(db.Boolen, default=False, nullable=False)
+    created_at = db.Column(db.DateTime, default= lambda: datetime.now(timezone.utc), nullable=False)
+    expirats_at = db.Column(db.Datetime, nullable=False)
 
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False, index=True)
     user = db.relationship('User', backref='reset_tokens')
+
+    def is_valid(self):
+        """Checks if token has not expired and has not been used yet"""
+        return not self.is_used and datetime.now(timezone.utc)
+
+    def __repr__(self):
+        return f"<PasswordResetToken user_id+{self.user_id} used={self.is_used}>"
