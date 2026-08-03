e26a48c5297a2824708de79db32255ed56e56dd2
yeswadams <yeswaadams73@gmail.com>
2026-08-03 05:30:50 +0300

    implemented the userSchema and its specialized schemas for handling http endpoints

---
+++ backend/app/features/auth/schemas/user_schema.py
@@ -0,0 +1,54 @@
+from app.features.auth.models import User
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow import Schema, fields, validate
+from app.extensions.database import db
+
+class UserSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+        sqla_session = db.session
+        load_instance = True 
+        include_fk = True
+        sqla_session = None
+    
+    id = fields.Integer(dump_only=True)
+    username = fields.String(
+        required=True, 
+        validate=[
+            validate.Length(min=3, max=50, error='Username must be between 3 and 50 characters'),
+            validate.Regexp(
+                r'^[a-zA-z0-9_]+$',
+                error="Username can only contain letters, numbers and underscores"
+            )
+        ]
+    )
+    passwordhash= fields.String(
+        required=True, 
+        load_only=True, 
+        validate=validate.Length(min=8, error="Password must be at least 8 characters")
+    )
+
+    created_at = fields.DateTime(dump_only=True)
+
+class UserRegisterSchema(Schema):
+    """Schema for POST /auth/register payloads"""
+    username = fields.Str(
+        required=True,
+        validate=validate.Length(min=3, max=50)
+    )
+    password = fields.Str(
+        required=True,
+        validate=validate.Length(min=8)
+    )
+
+class UserLoginSchema(Schema):
+    """Schema for POST /auth/login payloads"""
+    username = fields.Str(required=True)
+    password = fields.Str(required=True)
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+user_register_schema = UserRegisterSchema()
+user_login_schema = UserLoginSchema()
+    
